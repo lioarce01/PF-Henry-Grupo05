b5030e773b78e957225b188fa34a6d5342ca17b6
bautt-s <bauti_hora@hotmail.com>
2022-11-07 20:48:34 -0300

    added redux actions and reducers for users, ongs and posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,31 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsersAction, getUserByIdAction, getUserByNameAction } from './redux/reducers/dataBack/manageUsers/manageUsersActions';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.manageUsers.users);
+  const details = useSelector(state => state.manageUsers.details);
+
+  useEffect(() => {
+    dispatch(getUsersAction());
+    dispatch(getUserByIdAction(57));
+    dispatch(getUserByNameAction('old'));
+  }, [])
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="*" element='' />
-        <Route path="/" element='' />
-        <Route exact path="/home" element=""/>
-        <Route exact path="/landing" element="" />
-        <Route path="/:OngName/:post_id" element="" />
-      </Routes>
+      <h1>Hola!</h1>
+      <ul>
+          {users && users.map(e => {
+            return <li>
+              {e.nombre}
+            </li>
+          })}
+      </ul>
+
+      <p>{details.nombre}</p>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
