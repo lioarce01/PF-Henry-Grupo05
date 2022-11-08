6d79ddbce787b33352408787bb7e7ad01f960b09
lioarce01 <lioarce1@gmail.com>
2022-11-08 16:54:45 -0300

    feat(src/components/): home & home nav added

--- client/src/components/loginButton/index.jsx
+++ client/src/components/loginButton/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 const LoginButton = () => {
   return (
-    <Link to='/home' className="inline-flex items-center px-10 py-1 bg-gray-100 hover:bg-gray-300 text-gray-600  font-medium rounded-md shadow-sm">
-      login
+    <Link to='/home' className="inline-flex items-center px-10 py-1 font-medium text-gray-600 bg-gray-200 rounded-md shadow-lg hover:bg-gray-300">
+      Login
     </Link>
   );
 };
