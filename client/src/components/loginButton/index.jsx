8082e5498b694e53e1ecb4de469c7f9da9971a88
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-08 13:54:43 -0300

    style(client/src/components/loginButton): change bg-gray-200 to 100

--- client/src/components/loginButton/index.jsx
+++ client/src/components/loginButton/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const LoginButton = () => {
   return (
-    <Link to='/home' className="inline-flex items-center px-10 py-1 bg-gray-200 hover:bg-gray-300 text-gray-600  font-medium rounded-md shadow-lg">
+    <Link to='/home' className="inline-flex items-center px-10 py-1 bg-gray-100 hover:bg-gray-300 text-gray-600  font-medium rounded-md shadow-sm">
       login
     </Link>
   );
