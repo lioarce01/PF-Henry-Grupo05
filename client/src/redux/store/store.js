9ac2571d035d72179a0110839449d9f5afc8ed3a
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-08 18:51:03 -0300

    fix(src/redux/store): fix imports

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import manageUsersSlice from "../reducers/dataBack/manageUsers/manageUsersSlice"; 
-import managePostSlice from "../reducers/dataBack/manageUsers/manageUsersSlice"; 
-import manageOngsSlice from "../reducers/dataBack/manageUsers/manageUsersSlice"; 
+import managePostsSlice from "../reducers/dataBack/managePosts/managePostsSlice"; 
+import manageOngsSlice from "../reducers/dataBack/manageOngs/manageOngsSlice"; 
 
 export default configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
     reducer: {
         manageUsers: manageUsersSlice,
-        managePosts: managePostSlice,
+        managePosts: managePostsSlice,
         manageOngs: manageOngsSlice
     }
 })
