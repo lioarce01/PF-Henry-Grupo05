60c5feded5809efd2c7f31d7171b6433aa71fa42
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-08 13:34:09 -0300

    fix(src/components/loginButton): change login to Login

--- client/src/components/loginButton/index.jsx
+++ client/src/components/loginButton/index.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LoginButton = () => {
   return (
     <Link to='/home' className="inline-flex items-center px-10 py-1 bg-gray-200 hover:bg-gray-300 text-gray-600  font-medium rounded-md shadow-lg">
-      login
+      Login
     </Link>
   );
 };
