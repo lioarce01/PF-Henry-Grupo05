3efe9557f70386aa0f56a7bb17ef84555517ad5e
lioarce01 <lioarce1@gmail.com>
2022-11-09 13:32:46 -0300

    fix(src/components/navbar): dark mode button changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
+import {BsSun} from 'react-icons/bs'
+import {HiMoon} from 'react-icons/hi'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
   return (
     <div className='flex items-center justify-between w-full p-4 text-white bg-gray-800 shadow-lg h-14'>
       <div>
@@ -12,7 +20,11 @@ const Navbar = () => {
         <SearchBar/>
       </div>
       <div className='flex items-center justify-center'>
-        <button className='pt-1 pb-1 pl-2 pr-2 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300'>DarkMode</button>
+        {
+          toggle 
+          ? <button className='px-2 py-2 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><BsSun className='text-xl'/></button>
+          : <button className='px-2 py-2 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><HiMoon className='text-xl'/></button>
+        }
         <Link to="/home" className='pt-1 pb-1 pl-2 pr-2 mr-4 border-2 rounded-md hover:bg-white hover:text-black duration 300'>Home</Link>
         <Link to="/login" className='pt-1 pb-1 pl-2 pr-2 border-2 rounded-md hover:bg-white hover:text-black duration 300'>Login</Link>
       </div>
