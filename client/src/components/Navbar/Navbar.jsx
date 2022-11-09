29ca210d22d7aefc1308698ef63d03e247ffbb9f
lioarce01 <lioarce1@gmail.com>
2022-11-09 13:33:52 -0300

    fix(src/components/navbar): dark mode button changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
       <div className='flex items-center justify-center'>
         {
           toggle 
-          ? <button className='px-2 py-2 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><BsSun className='text-xl'/></button>
-          : <button className='px-2 py-2 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><HiMoon className='text-xl'/></button>
+          ? <button className='px-1.5 py-1.5 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><BsSun className='text-xl'/></button>
+          : <button className='px-1.5 py-1.5 mr-4 transition border-2 rounded-md hover:bg-white hover:text-black duration 300' onClick={handleToggle}><HiMoon className='text-xl'/></button>
         }
         <Link to="/home" className='pt-1 pb-1 pl-2 pr-2 mr-4 border-2 rounded-md hover:bg-white hover:text-black duration 300'>Home</Link>
         <Link to="/login" className='pt-1 pb-1 pl-2 pr-2 border-2 rounded-md hover:bg-white hover:text-black duration 300'>Login</Link>
