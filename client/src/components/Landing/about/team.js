9e2aa827813fd88539cab962645377ade997ea52
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-09 17:55:10 -0300

    feat(client/src/components/Landing/about/team.js): added data team

---
+++ client/src/components/Landing/about/team.js
@@ -0,0 +1,68 @@
+const team = [
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "https://github.com/",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+    {
+      name: "Nombre Apellido",
+      phrase: "frase",
+      image:
+        "https://i.picsum.photos/id/663/300/300.jpg?hmac=0FqSItoYfWTkZqdWY81yKAdMaxp9wsIiJcTj8D14BoA",
+      linkedin: "#",
+      github: "#",
+    },
+  ];
+
+  export default team
