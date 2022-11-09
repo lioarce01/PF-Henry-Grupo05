a533b7f80700b7f884e8b7c3e5e91ae739eee81a
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-09 17:56:30 -0300

    feat(client/src/App): import landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import OngDetail from "./components/OngProfile/OngProfile";
 import Home from "./components/Home/Home";
+import Landing from "./components/Landing";
+
 
 function App() {
   return (
     <div className="App">
+      
       <Routes>
         <Route path="*" element='' />
-        <Route path="/" element='' />
+        <Route path="/" element={<Landing />} />
         <Route exact path="/home" element={<Home />} />
-        <Route exact path="/landing" element="" />
         <Route path="/:OngName/:post_id" element="" />
         <Route path= "/:OngId/profile" element={<OngDetail/>}/>
       </Routes>
