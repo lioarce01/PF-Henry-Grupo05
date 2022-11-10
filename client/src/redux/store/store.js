35c26c0fa89e7c66d469aeb7a081c55d495f9523
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-10 11:12:38 -0300

    feat(client/src/redux/store): added loading reducer

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -3,6 +3,7 @@ import manageUsersSlice from "../reducers/dataBack/manageUsers/manageUsersSlice"
 import managePostsSlice from "../reducers/dataBack/managePosts/managePostsSlice"; 
 import manageSheltersSlice from "../reducers/dataBack/manageShelters/manageSheltersSlice"; 
 import manageCommentsSlice from '../reducers/dataBack/manageComments/manageCommentsSlice';
+import loadingSlice  from '../reducers/dataBack/loading/loadingSlice';
 
 export default configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
@@ -10,6 +11,7 @@ export default configureStore({
         manageUsers: manageUsersSlice,
         managePosts: managePostsSlice,
         manageComments: manageCommentsSlice,
-        manageShelters: manageSheltersSlice
+        manageShelters: manageSheltersSlice,
+        loading: loadingSlice
     }
 })
