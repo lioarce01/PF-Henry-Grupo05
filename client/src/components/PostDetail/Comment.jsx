1199afba3e84c6c80a563fa5adec4e5aa7aebbf0
lioarce01 <lioarce1@gmail.com>
2022-11-11 17:39:44 -0300

    minor fixes

--- client/src/components/PostDetail/Comment.jsx
+++ client/src/components/PostDetail/Comment.jsx
@@ -25,7 +25,7 @@ const Comment = ({ content, author, id, postId}) => {
 
   return (
     <div className="flex flex-col mx-auto">
-      <div className="w-[100%] max-w-xl my-4 min-w-[500px] bg-white border border-gray-200 rounded-lg shadow-md mx-auto">
+      <div className="w-[100%] max-w-xl my-4 md:min-w-[500px] bg-white border border-gray-200 rounded-lg shadow-md mx-auto">
         <div className="flex flex-col px-4 py-6">
           <div className="flex items-end justify-end w-full ">
             <button
@@ -42,84 +42,17 @@ const Comment = ({ content, author, id, postId}) => {
               alt="avatar"
             />
             <div className="w-full">
-                <div className="flex flex-row items-start justify-between w-full mb-4">
-                  <div> 
-                    <h2 className="-mt-1 text-lg font-semibold text-gray-900">
-                      <Link className="object-cover w-12 h-12 mr-4 rounded-full">
-                        {author.name}
-                      </Link>
-                    </h2>
-                    <small className="text-sm text-gray-700">{author.role}</small>
-                  </div>
-                  <div>
-                    <div className="fixed inset-0 flex items-center justify-center">
-                      <button
-                        type="button"
-                        onClick={openModal}
-                        className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                      >
-                        Open dialog
-                      </button>
-                    </div>
-
-                    <Transition appear show={isOpen} as={Fragment}>
-                      <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0"
-                          enterTo="opacity-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100"
-                          leaveTo="opacity-0"
-                        >
-                          <div className="fixed inset-0 bg-black bg-opacity-25" />
-                        </Transition.Child>
-
-                        <div className="fixed inset-0 overflow-y-auto">
-                          <div className="flex items-center justify-center min-h-full p-4 text-center">
-                            <Transition.Child
-                              as={Fragment}
-                              enter="ease-out duration-300"
-                              enterFrom="opacity-0 scale-95"
-                              enterTo="opacity-100 scale-100"
-                              leave="ease-in duration-200"
-                              leaveFrom="opacity-100 scale-100"
-                              leaveTo="opacity-0 scale-95"
-                            >
-                              <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                                <Dialog.Title
-                                  as="h3"
-                                  className="text-lg font-medium leading-6 text-gray-900"
-                                >
-                                  Payment successful
-                                </Dialog.Title>
-                                <div className="mt-2">
-                                  <p className="text-sm text-gray-500">
-                                    Your payment has been successfully submitted. We’ve sent
-                                    you an email with all of the details of your order.
-                                  </p>
-                                </div>
-
-                                <div className="mt-4">
-                                  <button
-                                    type="button"
-                                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                    onClick={closeModal}
-                                  >
-                                    Got it, thanks!
-                                  </button>
-                                </div>
-                              </Dialog.Panel>
-                            </Transition.Child>
-                          </div>
-                        </div>
-                      </Dialog>
-                    </Transition>
-                  </div>
+              <div className="flex flex-row items-start justify-between w-full mb-4">
+                <div> 
+                  <h2 className="-mt-1 text-lg font-semibold text-gray-900">
+                    <Link className="object-cover w-12 h-12 mr-4 rounded-full">
+                      {author.name}
+                    </Link>
+                  </h2>
+                  <small className="text-sm text-gray-700">{author.role}</small>
                 </div>
-
-              <p className="text-sm text-left text-gray-700">{content}</p>
+             </div>
+                <p className="text-sm text-left text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ut odit hic! Id, rerum quas exercitationem hic perferendis neque minus minima eos aliquid esse suscipit incidunt vero facere, optio nam.</p>
             </div>
           </div>
         </div>
