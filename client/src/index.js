33aa7bbd144ce6a1fab286e46829c44264b33107
lioarce01 <lioarce1@gmail.com>
2022-11-12 20:32:11 -0300

    styled(src/components/userProfile): restyled user profile)

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store} >
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
   </Provider>
 );
