2767d1d8803a1c456293411339dcb8be6274a728
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-11 22:29:05 -0300

    feat(client/src/components/home/):  added modal in Posts component

--- client/src/components/Home/Posts.jsx
+++ client/src/components/Home/Posts.jsx
@@ -1,27 +1,43 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { getPostsAction } from '../../redux/reducers/dataBack/managePosts/managePostsActions'
-import CardPost from './CardPost'
-import PostFilters from './PostFilters'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostsAction } from "../../redux/reducers/dataBack/managePosts/managePostsActions";
+import CardPost from "./CardPost";
+import PostFilters from "./PostFilters";
+import ModalCreatePost from "./ModalCreatePost";
 
 const Posts = () => {
-  const dispatch = useDispatch()
-  const posts = useSelector(state => state.managePosts.posts);
-  console.log(posts)
+  let [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.managePosts.posts);
+
+  const closeModal = () => setIsOpen(false);
 
   useEffect(() => {
-    dispatch(getPostsAction())
-  }, [dispatch])
+    dispatch(getPostsAction());
+  }, [dispatch]);
 
   return (
-    <div className='w-full min-h-[50rem] px-32 py-10 mb-4 mt-14 bg-slate-200'>
-      <div className='flex items-center justify-end'>
-        <PostFilters/>
+    <div className="w-full min-h-[50rem] px-32 py-10 mb-4 mt-14 bg-slate-200">
+      <div className="flex flex-col w-full">
+        <div className="flex w-full">
+        <button
+          onClick={() => setIsOpen(true)}
+          class="bg-transparent hover:bg-[#462312] text-[#462312] font-semibold hover:text-white py-1 px-4 border border-[#462312] hover:border-transparent rounded mx-auto"
+        >
+          Create Post
+        </button>
+        </div>
+        
+
+        <div className="flex items-center justify-end">
+          <PostFilters />
+        </div>
+
       </div>
-      <div className=''>
-        {
-          posts && posts.map(post => {
+      <div className="">
+        {posts &&
+          posts.map((post) => {
             return (
               <CardPost
                 key={post.id}
@@ -34,12 +50,12 @@ const Posts = () => {
                 comments={post.Comment.length}
                 authorId={post.authorId}
               />
-            )
-          })
-        }
+            );
+          })}
       </div>
+      <ModalCreatePost isOpen={isOpen} closeModal={closeModal} />
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
