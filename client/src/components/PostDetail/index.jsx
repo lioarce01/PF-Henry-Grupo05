a50af5d1a98de640024cff7a1518b71c86eac9d8
bautt-s <bauti_hora@hotmail.com>
2022-11-13 18:00:56 -0300

    feat(client/src/components/home/postDetail/...): added like button interaction

--- client/src/components/PostDetail/index.jsx
+++ client/src/components/PostDetail/index.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPostsByIdAction, cleanDetailsAction} from "../../redux/reducers/dataBack/managePosts/managePostsActions";
+import { getPostsByIdAction, cleanDetailsAction } from "../../redux/reducers/dataBack/managePosts/managePostsActions";
 import { selectPost } from "../../redux/reducers/dataBack/managePosts/managePostsSlice";
 
 import ModalEditPost from "./ModalEditPost";
-import Comments  from "./Comments";
+import Comments from "./Comments";
 import AuthorData from "./AuthorData";
 import PostData from "./PostData";
 
-const Post = ({ postId, closeModal }) => {
-  
+const Post = ({ postId, closeModal, setLike, like }) => {
+
   const { details } = useSelector(selectPost);
   const dispatch = useDispatch();
   const [toogle, setToogle] = useState(true)
@@ -25,12 +25,13 @@ const Post = ({ postId, closeModal }) => {
 
   return (
     <div className="flex flex-col w-[50rem]  p-6 space-y-6 overflow-hidden rounded-lg shadow-md  bg-[#FAF2E7] border border-gray-200 mx-auto">
-      <div className="flex space-x-4">
-       <AuthorData details={details} />
-       <ModalEditPost setToogle={setToogle} postId={postId}/>
+      <div className="flex flex-row space-x-4">
+        <AuthorData details={details} />
+        <ModalEditPost setToogle={setToogle} postId={postId} />
       </div>
-      <PostData closeModal={closeModal} toogle={toogle} postId={postId}   details={details} />
-      <Comments postId={postId} details={details}/>
+      
+      <PostData closeModal={closeModal} toogle={toogle} postId={postId} details={details} setLike={setLike} like={like} />
+      <Comments postId={postId} details={details} />
     </div>
   );
 };
