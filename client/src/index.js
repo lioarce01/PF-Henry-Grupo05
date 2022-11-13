a41d2650730249104fa60690524f8bb77f287b9c
lioarce01 <lioarce1@gmail.com>
2022-11-13 02:50:52 -0300

    feat(src/routes/posts): added get posts by most recent

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store/store';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
