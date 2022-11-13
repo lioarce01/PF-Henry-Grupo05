92edf475aeb6ffc593772443748afebb3f33017d
Lionel <45451118+lioarce01@users.noreply.github.com>
2022-11-13 14:56:10 -0300

    Update OngForm.jsx

--- client/src/components/OngForm/OngForm.jsx
+++ client/src/components/OngForm/OngForm.jsx
@@ -7,6 +7,8 @@ import Navbar from '../Navbar/Navbar';
 import {createSheltersAction} from "../../redux/reducers/dataBack/manageShelters/manageSheltersActions";
 import UploadImage from './UploadImage';
 
+
+
 const OngForm = () => {
     const [image, setImage] = useState('');
     const dispatch = useDispatch();
@@ -14,7 +16,6 @@ const OngForm = () => {
 
     const onSubmit = async () =>{
       if(image.length){
-
 
         console.log('submitted')
         console.log({...values, profilePic: image});
@@ -51,28 +52,28 @@ const OngForm = () => {
             <div className='flex flex-row w-full p-14 h-full' >
               <div className='flex flex-col w-full h-full'>
                 <div className="mb-6 w-full ">
-                  <label className="block mb-2 text-xl font-medium text-black dark:text-black after:content-['*'] after:ml-0.5 after:text-red-500">Name</label>
+                  <label className="block mb-2 text-xl font-medium text-black dark:text-black">Name: </label>
                   <div className="flex items-center justify-arround w-full">
                     <input type="text" value={values.name} name= 'name'  placeholder='Add name...' onChange={handleChange} onBlur={handleBlur} className="bg-gray-50 border w-2/4 border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg[#EEEEE6] dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     {errors.name && <p className='text-red-500'>  {errors.name}</p>}
                   </div>
                 </div>
                 <div className="mb-6 w-full">
-                  <label className="block mb-2 text-xl font-medium text-black dark:text-black after:content-['*'] after:ml-0.5 after:text-red-500">Country</label>
+                  <label className="block mb-2 text-xl font-medium text-black dark:text-black">Country: </label>
                   <div className="flex items-center">
                     <input type="text" value={values.country} name= 'country'  placeholder='Add country..' onChange={handleChange} onBlur={handleBlur}  className="bg-gray-50 w-2/4 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg[#EEEEE6]  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     {errors.country && <p className='text-red-500 '>{errors.country}</p>}   
                   </div>
                 </div>
                 <div className="mb-6 w-full">
-                  <label className="block mb-2 text-xl font-medium text-black dark:text-black after:content-['*'] after:ml-0.5 after:text-red-500">City</label>
+                  <label className="block mb-2 text-xl font-medium text-black dark:text-black">City: </label>
                   <div className="flex items-center">
                     <input type="text" value={values.city} name= 'city'  placeholder='Add city..' onChange={handleChange} onBlur={handleBlur}  className="bg-gray-50 w-2/4 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg[#EEEEE6]  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     {errors.city && <p className='text-red-500'>{errors.city}</p>}   
                   </div>
                 </div>
                 <div className="mb-6 w-full">
-                  <label className="block mb-2 text-xl font-medium text-black dark:text-black after:content-['*'] after:ml-0.5 after:text-red-500">Address</label>
+                  <label className="block mb-2 text-xl font-medium text-black dark:text-black">Address: </label>
                   <div className="flex items-center">
                     <input type="text" value={values.address} name= 'address'  placeholder='Add address..' onChange={handleChange} onBlur={handleBlur}  className="bg-gray-50 w-2/4 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg[#EEEEE6]  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     {errors.address && <p className='text-red-500'>{errors.address}</p>}   
@@ -86,7 +87,7 @@ const OngForm = () => {
                   </div>
                 </div>
                 <div className="mb-6 w-full">
-                  <label className="block mb-2 text-xl font-medium text-black dark:text-black after:content-['*'] after:ml-0.5 after:text-red-500">Goal</label>
+                  <label className="block mb-2 text-xl font-medium text-black dark:text-black">Goal: </label>
                   <div className="flex items-center">
                     <input type="number" value={values.goal} name= 'goal' min='1' placeholder='Add goal..' onChange={handleChange} onBlur={handleBlur}  className="bg-gray-50 w-2/4 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg[#EEEEE6]  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"/> 
                     {errors.goal && <p className='text-red-500'>{errors.goal}</p>} 
@@ -111,8 +112,8 @@ const OngForm = () => {
               <div className="grid grid-cols-1 space-y-2">
                 <div>
 
-                  <label className="text-xl  font-bold text-black tracking-wide after:content-['*'] after:ml-0.5 after:text-red-500">
-                    Description
+                  <label className="text-xl  font-bold text-black tracking-wide">
+                    Description:
                   </label>
                   <textarea
                     name= 'description'
@@ -142,39 +143,10 @@ const OngForm = () => {
                 </div>
             }
 
-								<div>
-									<UploadImage image={image} setImage={setImage} />
-								</div>
-							</div>
-						</div>
-					</div>
-					{values.name !== "" &&
-					Object.entries(errors).length === 0 &&
-					values.description !== "" &&
-					values.address !== "" &&
-					values.city !== "" &&
-					values.country !== "" ? (
-						<div>
-							<button
-								type="submit"
-								className="pt-1 pb-1 pl-2 w-32 pr-2 mr-4 mt-0 text-xl transition bg-[#FAF2E7] border border-[#ca7c62] rounded-md hover:bg-[#ca7c62] border border-[#fffefe] hover:text-black duration 300">
-								Submit
-							</button>
-						</div>
-					) : (
-						<div>
-							<button
-								type="submit"
-								className="pt-1 pb-1 pl-2 pr-2 mr-4 transition border border-white bg-[#FAF2E7] hidden rounded-md hover:bg-orange-100 hover:text-black duration 300"
-								disabled>
-								Submit
-							</button>
-						</div>
-					)}
-				</form>
-			</div>
-		</div>
-	)
+          </form>
+        </div>
+    </div>
+  )
 }
 
 export default OngForm;
