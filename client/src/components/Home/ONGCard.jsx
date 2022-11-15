a7a0a5c193e53e05cccbdcc4b4edb17cec2dd39a
lioarce01 <lioarce1@gmail.com>
2022-11-15 18:57:56 -0300

    added show more text to shelter cards

--- client/src/components/Home/ONGCard.jsx
+++ client/src/components/Home/ONGCard.jsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import ShowMoreText from "react-show-more-text"
 
 const ONGCard = ({ image, name, goal, budget, followers, description, id }) => {
 	if (!image) {
 		image =
 			"https://media.4-paws.org/6/8/9/3/689354d6694789b45569cd647a6009e240b4afe7/VIER%20PFOTEN_2016-09-18_081-1927x1333-1920x1328.jpg"
 	}
+
+	const executeOnClick = (isExpanded) => {
+		console.log(isExpanded)
+	}
+
 	return (
 		<div className="flex flex-col items-center justify-center m-4">
-			<div className="max-w-[360px] transition duration-300 bg-[#fffcf7] border-2 border-[#fffcf7] rounded-lg">
+			<div className="max-w-[360px] transition duration-300 bg-[#fffcf7] border-2 border-[#eeebe6] rounded-lg">
 				<div>
 					<img className="rounded-t-lg" src={image} alt="ONG" />
 				</div>
@@ -20,7 +26,18 @@ const ONGCard = ({ image, name, goal, budget, followers, description, id }) => {
 					</div>
 					<div className="flex flex-col justify-start w-full">
 						<p className="mb-3 font-semibold text-gray-800 text-md">
-							{description}
+							<ShowMoreText
+								lines={3}
+								more="Show more"
+								less="Show less"
+								className="content-css"
+								anchorClass="show-more-less-clickable"
+								onClick={executeOnClick}
+								expanded={false}
+								// width={500}
+								truncatedEndingComponent={"... "}>
+								{description}
+							</ShowMoreText>
 						</p>
 						<p className="text-sm font-semibold">
 							Budget: <span className="font-semibold">${budget}</span>
