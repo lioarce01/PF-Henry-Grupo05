c8c9a2b2d1eb381cdac74c9ffea30c24cbc04074
nllosas <nllosas@fi.uba.ar>
2022-11-15 16:59:27 -0300

    adding functions to login button

--- client/src/components/Navbar/LoginButton.jsx
+++ client/src/components/Navbar/LoginButton.jsx
@@ -2,10 +2,15 @@ import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 
 const LoginButton = () => {
-	const { loginWithPopup } = useAuth0()
+	const { loginWithPopup, user } = useAuth0()
+
+	const manageClick = async () => {
+		await loginWithPopup()
+		
+	}
 	return (
 		<div>
-			<button onClick={() => loginWithPopup()}>Log In</button>
+			<button onClick={manageClick}>Log In</button>
 		</div>
 	)
 }
