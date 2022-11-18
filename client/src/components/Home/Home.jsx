150386199083a25c0abce3b83459d10dcc1dc36d
lioarce01 <lioarce1@gmail.com>
2022-11-18 17:16:57 -0300

    fixed bg home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -7,22 +7,22 @@ import { Toaster } from 'react-hot-toast'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#fff5f4] overflow-y-hidden'>
-      <Toaster />
-      <Navbar />
-      <div className='flex flex-row justify-between pt-14'>
-        <div>
-          <SubscriptorsBar />
-        </div>
-        <div>
-          <Posts />
-        </div>
-        <div>
-          <Ongs />
-        </div>
-      </div>
-    </div>
-  )
+		<div className="w-full h-full bg-[#fff5f4] min-h-screen overflow-y-hidden">
+			<Toaster />
+			<Navbar />
+			<div className="flex flex-row justify-between pt-14">
+				<div>
+					<SubscriptorsBar />
+				</div>
+				<div>
+					<Posts />
+				</div>
+				<div>
+					<Ongs />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Home
