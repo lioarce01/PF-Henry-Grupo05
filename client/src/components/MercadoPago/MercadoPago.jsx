7afd65c385c36f7e389a1e0ea7d2b4ba72ad790e
JuanjoPischetz <juanjo.pischetz@gmail.com>
2022-11-21 00:06:02 -0300

    MercadoPago Styled

--- client/src/components/MercadoPago/MercadoPago.jsx
+++ client/src/components/MercadoPago/MercadoPago.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
-import axios from "axios"
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import Spinner from '../Spinner/Spinner'
 import Swal from "sweetalert2"
 import { useLazyVerifyPaymentQuery } from '../../redux/api/mercadopago'
 
@@ -12,7 +9,6 @@ const MercadoPago = () => {
     const [verify] = useLazyVerifyPaymentQuery()
 
     const navigate = useNavigate()
-    const dispatch = useDispatch()
 
     useEffect(() => {
         const query = new URLSearchParams(window.location.search);
@@ -28,11 +24,22 @@ const MercadoPago = () => {
     }, [])
 
   return (
-    <div>
-      <p className='text-center text-5xl'>Estamos procesando tu pago...</p>
-      <p className='text-center text-5xl'>Por favor espere un momento...</p>
-      <img src="https://logotipoz.com/wp-content/uploads/2021/10/version-horizontal-large-logo-mercado-pago.webp" alt="" className='h-40 w-72 mx-auto mt-5 mb-28'/>
-      <Spinner />
+    <div className='flex flex-col w-screen h-screen place-content-center'>
+    <div className='flex flex-col h-fit w-1/3 mx-auto p-5 bg-white shadow-xl shadow-[rgb(255,213,201)] rounded-2xl'>
+      <div className='mb-16'>
+      <p className='text-center text-4xl text-[#3D190C] font-bold'>Thanks for Helping Us to make a Better World!</p>
+      </div>
+      <div className='flex flex-row items-center justify-around h-fit mb-16'>
+      <div className="flex flex-row ">
+        <h1 className=" font-bold text-4xl text-[#3D190C]">Paws</h1>
+        <h1 className=" font-bold text-4xl text-[#F87171]">Founding</h1>
+      </div>
+      <img src="https://logotipoz.com/wp-content/uploads/2021/10/version-horizontal-large-logo-mercado-pago.webp" alt="mercado_pago_logo" 
+      className='h-32 w-60 '/>
+      </div>
+      <p className='text-center text-2xl text-[#3D190C] mb-20'>Thanks to Heroes like you, we can continue rescuing animals until we find them a home.</p>
+      <p className='text-center text-1xl text-[#F87171]'>Wait a moment please, will redirected soon...</p>
+    </div>
     </div>
   )
 }
