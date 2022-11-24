d1850140b143fab75255c02fda6090e9e18a50f0
lioarce01 <lioarce1@gmail.com>
2022-11-24 17:52:18 -0300

    feat/progress-bar added

--- client/src/components/Goals/BtnCreate.jsx
+++ client/src/components/Goals/BtnCreate.jsx
@@ -6,11 +6,16 @@ function BtnCreate({shelterId}) {
     const [isOpen, setIsOpen] = useState(false)
     const closeModal = () => setIsOpen(false)
   return (
-    <>
-    <button className='border border-gray-500 px-2 py-1' onClick={() => setIsOpen(true)}>Create goal</button>
-    <Modal isOpen={isOpen} closeModal={closeModal} shelterId={shelterId} />
-    </>
-  )
+		<>
+			<button
+				className="px-2 py-1 text-white transition duration-300 border-2 rounded-md border-rose-400 bg-rose-400 hover:bg-rose-500 hover:border-rose-500"
+				onClick={() => setIsOpen(true)}
+			>
+				Create goal
+			</button>
+			<Modal isOpen={isOpen} closeModal={closeModal} shelterId={shelterId} />
+		</>
+	)
 }
 
 export default BtnCreate
