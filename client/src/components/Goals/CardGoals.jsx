8c07ce33fa03eac18e0da8af4ac7af759cb0db07
lioarce01 <lioarce1@gmail.com>
2022-11-24 22:52:32 -0300

    fixed goal width & shelter img width and height

--- client/src/components/Goals/CardGoals.jsx
+++ client/src/components/Goals/CardGoals.jsx
@@ -17,34 +17,32 @@ const CardGoals = ({
 	}
 
 	return (
-		<div>
-			<div className="flex flex-col gap-2 p-3 my-2 bg-white shadow-lg rounded-xl shadow-rose-400 w-fit">
-				<h1 className="text-lg font-bold">{title}</h1>
-				<p className="text-gray-700">{content}</p>
-				<div className="flex flex-col items-center">
-					<label htmlFor="goal" className="flex flex-row items-center gap-1">
-						{budget / 100}% <span>completed</span>
-					</label>
-					<div className="relative inline-block bg-white h-[20px] rounded-lg overflow-hidden">
-						<progress id="goal" max={goal} value={budget}></progress>
-					</div>
+		<div className="flex flex-col w-full gap-2 p-3 my-2 bg-white shadow-lg rounded-xl shadow-rose-400">
+			<h1 className="text-lg font-bold">{title}</h1>
+			<p className="text-gray-700">{content}</p>
+			<div className="flex flex-col items-center">
+				<label htmlFor="goal" className="flex flex-row items-center gap-1">
+					{budget / 100}% <span>completed</span>
+				</label>
+				<div className="relative inline-block bg-white h-[20px] rounded-lg overflow-hidden">
+					<progress id="goal" max={goal} value={budget}></progress>
 				</div>
-				<div className="flex items-center justify-center w-full">
-					<button
-						onClick={() => setIsOpenDonate(true)}
-						className="px-2 py-1 text-white transition duration-300 rounded-md bg-rose-400 hover:bg-rose-500"
-					>
-						Help us!
-					</button>
-				</div>
-				<ModalDonate
-					isOpen={isOpenDonate}
-					closeModal={closeModalDonate}
-					name={shelterName}
-					goalId={id}
-					shelterId={shelterId}
-				/>
 			</div>
+			<div className="flex items-center justify-center w-full">
+				<button
+					onClick={() => setIsOpenDonate(true)}
+					className="px-2 py-1 text-white transition duration-300 rounded-md bg-rose-400 hover:bg-rose-500"
+				>
+					Help us!
+				</button>
+			</div>
+			<ModalDonate
+				isOpen={isOpenDonate}
+				closeModal={closeModalDonate}
+				name={shelterName}
+				goalId={id}
+				shelterId={shelterId}
+			/>
 		</div>
 	)
 }
--- client/src/components/OngProfile/OngFormUpdate.jsx
+++ client/src/components/OngProfile/OngFormUpdate.jsx
@@ -74,184 +74,216 @@ const OngFormUpdate = ({
   console.log(errors);
 
   return (
-		<div className="flex mt-10 lg:mt-4 flex-col items-center w-9/12 lg:mt-0 lg:w-full">
-			<div className="h-fit w-full mb-2 lg:p-0 lg:min-w-0 lg:w-full lg:max-w-full">
-				
-					<div className="bg-white shadow-[rgba(255,213,201)] shadow-xl p-2 rounded-2xl lg:flex lg:items-center lg:flex-col
-					lg:w-full lg:p-0 lg:border-2 lg:border-blue lg:w-full lg:max-w-full lg:min-w-[250px]">
-						{
-							!toggle ? <UploadImage 
-							image={image} setImage={setImage} toggle={toggle}
-							className='lg:w-full lg:min-w-[250px]' /> 
-							: <img
-							className="object-cover rounded-2xl lg:w-full lg:p-1"
+		<div className="flex flex-col items-center w-9/12 mt-10 lg:mt-4 lg:mt-0 lg:w-full">
+			<div className="w-full mb-2 h-fit lg:p-0 lg:min-w-0 lg:w-full lg:max-w-full">
+				<div
+					className="bg-white shadow-[rgba(255,213,201)] shadow-xl p-2 rounded-2xl lg:flex lg:items-center lg:flex-col
+					lg:w-full lg:p-0 lg:border-2 lg:border-blue lg:w-full lg:max-w-full lg:min-w-[250px]"
+				>
+					{!toggle ? (
+						<UploadImage
+							image={image}
+							setImage={setImage}
+							toggle={toggle}
+							className="lg:w-full lg:min-w-[250px]"
+						/>
+					) : (
+						<img
+							className="object-cover rounded-2xl lg:w-full lg:p-1 w-[300px] h-[300px]"
 							src={details?.profilePic}
 							alt="avatar"
 						/>
-						}
-						<div className="min-w-full">
+					)}
+					<div className="min-w-full">
 						<form onSubmit={handleSubmit}>
-							<div className="text-lg text-center sm:text-2xl lg:text-lg font-bold text-black"> 
-							{!toggle && <input
-									className="my-2 bg-white min-w-full"
-									defaultValue={name}
-									type="text"
-									name="name"
-									onChange={handleChange}
-									value={values.name}
-									disabled={toggle}
-								/>}
-							{toggle && <p
-									className="my-2 bg-white min-w-full pl-2 cursor-default"
-									>{values.name}</p>}
+							<div className="text-lg font-bold text-center text-black sm:text-2xl lg:text-lg">
+								{!toggle && (
+									<input
+										className="min-w-full my-2 bg-white"
+										defaultValue={name}
+										type="text"
+										name="name"
+										onChange={handleChange}
+										value={values.name}
+										disabled={toggle}
+									/>
+								)}
+								{toggle && (
+									<p className="min-w-full pl-2 my-2 bg-white cursor-default">
+										{values.name}
+									</p>
+								)}
 								{errors.name && <p className="text-red-500"> {errors.name}</p>}
 							</div>
 						</form>
-						</div>
 					</div>
-					<div className="bg-white shadow-[rgba(255,213,201)] shadow-xl p-2 rounded-2xl mt-2 lg:w-full lg:flex lg:flex-col">
-						<form onSubmit={handleSubmit}>
-							<div className="flex flex-col text-xs sm:text-base font-semibold text-black lg:w-full">
-								<div className="flex flex-row items-baseline">
+				</div>
+				<div className="bg-white shadow-[rgba(255,213,201)] shadow-xl p-2 rounded-2xl mt-2 lg:w-full lg:flex lg:flex-col">
+					<form onSubmit={handleSubmit}>
+						<div className="flex flex-col text-xs font-semibold text-black sm:text-base lg:w-full">
+							<div className="flex flex-row items-baseline">
 								<label className="text-[#d45f37]">Country: </label>
-								{!toggle && <input
-									className="my-2 bg-white indent-4 w-full lg:my-1"
-									defaultValue={country}
-									type="text"
-									name="country"
-									onChange={handleChange}
-									value={values.country}
-									disabled={toggle}
-								/>}
-								{toggle && <p
-									className="my-2 bg-white w-full pl-4 cursor-default lg:my-1"
-									>{values.country}</p>}
+								{!toggle && (
+									<input
+										className="w-full my-2 bg-white indent-4 lg:my-1"
+										defaultValue={country}
+										type="text"
+										name="country"
+										onChange={handleChange}
+										value={values.country}
+										disabled={toggle}
+									/>
+								)}
+								{toggle && (
+									<p className="w-full pl-4 my-2 bg-white cursor-default lg:my-1">
+										{values.country}
+									</p>
+								)}
 								{errors.name && <p className="text-red-500"> {errors.name}</p>}
 								{errors.country && (
 									<p className="text-red-500"> {errors.country}</p>
 								)}
-								</div>
-								<div className="flex flex-row items-baseline">
+							</div>
+							<div className="flex flex-row items-baseline">
 								<label className="text-[#d45f37]">City: </label>
-								{!toggle && <input
-									className="my-2 bg-white indent-4 w-full lg:my-1"
-									defaultValue={city}
-									type="text"
-									name="city"
-									onChange={handleChange}
-									value={values.city}
-									disabled={toggle}
-								/>}
-								{toggle && <p
-									className="my-2 bg-white w-full pl-4 cursor-default lg:my-1"
-									>{values.city}</p>}
+								{!toggle && (
+									<input
+										className="w-full my-2 bg-white indent-4 lg:my-1"
+										defaultValue={city}
+										type="text"
+										name="city"
+										onChange={handleChange}
+										value={values.city}
+										disabled={toggle}
+									/>
+								)}
+								{toggle && (
+									<p className="w-full pl-4 my-2 bg-white cursor-default lg:my-1">
+										{values.city}
+									</p>
+								)}
 								{errors.city && <p className="text-red-500"> {errors.city}</p>}
-								</div>
-								<div className="flex flex-row items-baseline">
+							</div>
+							<div className="flex flex-row items-baseline">
 								<label className="text-[#d45f37]">Address: </label>
-								{!toggle && <input
-									className="my-2 bg-white indent-4 w-full lg:my-1"
-									defaultValue={address}
-									type="text"
-									name="address"
-									onChange={handleChange}
-									value={values.address}
-									disabled={toggle}
-								/>}
-								{toggle && <p
-									className="my-2 bg-white w-full pl-4 cursor-default lg:my-1"
-									>{values.address}</p>}
+								{!toggle && (
+									<input
+										className="w-full my-2 bg-white indent-4 lg:my-1"
+										defaultValue={address}
+										type="text"
+										name="address"
+										onChange={handleChange}
+										value={values.address}
+										disabled={toggle}
+									/>
+								)}
+								{toggle && (
+									<p className="w-full pl-4 my-2 bg-white cursor-default lg:my-1">
+										{values.address}
+									</p>
+								)}
 								{errors.address && (
 									<p className="text-red-500"> {errors.address}</p>
 								)}
-								</div>
-								
-								<div className="flex flex-row items-baseline w-full">
+							</div>
+
+							<div className="flex flex-row items-baseline w-full">
 								<label className="text-[#d45f37]">Website: </label>
-								{!toggle && <input
-									className="my-2 bg-white indent-4 w-full lg:my-1"
-									defaultValue={website}
-									type="text"
-									name="website"
-									onChange={handleChange}
-									value={values.website}
-									disabled={toggle}
-								/>}
-								{toggle && 
-									<Link to={{pathname: `http://${values.website}`}} target='_blank'
-									className='w-9/12 '>
-									<p
-									className="my-2 bg-white w-full pl-4 cursor-pointer lg:my-1 truncate"
-									>{values.website}</p>
-									</Link>}
+								{!toggle && (
+									<input
+										className="w-full my-2 bg-white indent-4 lg:my-1"
+										defaultValue={website}
+										type="text"
+										name="website"
+										onChange={handleChange}
+										value={values.website}
+										disabled={toggle}
+									/>
+								)}
+								{toggle && (
+									<Link
+										to={{ pathname: `http://${values.website}` }}
+										target="_blank"
+										className="w-9/12 "
+									>
+										<p className="w-full pl-4 my-2 truncate bg-white cursor-pointer lg:my-1">
+											{values.website}
+										</p>
+									</Link>
+								)}
 								{errors.website && (
 									<p className="text-red-500"> {errors.website}</p>
 								)}
-								</div>
 							</div>
-										{!toggle && Object.entries(errors).length === 0 && (
-											<button
-												type="submit"
-												className="text-xs sm:text-lg my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-2 px-4  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300">
-												Save
-											</button>
-										)}
-										{shelter.author.id === userDetail?.id && (
-											<button
-												className="text-xs sm:text-lg my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-2 px-4  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
-												onClick={() => setToggle(!toggle)}>
-												Edit
-											</button>
-										)}
-						</form>
-						{userDetail.role === "Admin" || shelter.author.id === userDetail.id ? (
-					<div className="flex flex-row w-full justify-end text-xs sm:text-sm">
-					<>
-						{shelter.enable ? (
+						</div>
+						{!toggle && Object.entries(errors).length === 0 && (
 							<button
-								onClick={() => disableShelter({ shelterId: id })}
-								className="bg-transparent hover:bg-[#d32727] bg-[#b90707] text-white transition duration-300 
-								font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded-lg flex flex-row 
-								items-center justify-center border lg:text-sm lg:border-transparent ">
-								Disable
-								<span className="pl-2">
-									<MdDomainDisabled />
-								</span>
-							</button>
-						) : (
-							<button
-								onClick={() => enableShelter({ shelterId: id })}
-								className="bg-transparent hover:bg-[#24c531] bg-[#22b92f] transition duration-300 text-white 
-								font-semibold hover:text-white py-2 px-4 border hover:border-transparent rounded-lg flex flex-row 
-								items-center justify-center lg:text-sm lg:border-transparent">
-								Enable
-								<span className="pl-2">
-									<MdOutlineDomain />
-								</span>
+								type="submit"
+								className="text-xs sm:text-lg my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-2 px-4  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
+							>
+								Save
 							</button>
 						)}
-					</>
-					</div>
-				) : null}
-					</div>
-				
+						{shelter.author.id === userDetail?.id && (
+							<button
+								className="text-xs sm:text-lg my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-2 px-4  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
+								onClick={() => setToggle(!toggle)}
+							>
+								Edit
+							</button>
+						)}
+					</form>
+					{userDetail.role === "Admin" ||
+					shelter.author.id === userDetail.id ? (
+						<div className="flex flex-row justify-end w-full text-xs sm:text-sm">
+							<>
+								{shelter.enable ? (
+									<button
+										onClick={() => disableShelter({ shelterId: id })}
+										className="bg-transparent hover:bg-[#d32727] bg-[#b90707] text-white transition duration-300 
+								font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded-lg flex flex-row 
+								items-center justify-center border lg:text-sm lg:border-transparent "
+									>
+										Disable
+										<span className="pl-2">
+											<MdDomainDisabled />
+										</span>
+									</button>
+								) : (
+									<button
+										onClick={() => enableShelter({ shelterId: id })}
+										className="bg-transparent hover:bg-[#24c531] bg-[#22b92f] transition duration-300 text-white 
+								font-semibold hover:text-white py-2 px-4 border hover:border-transparent rounded-lg flex flex-row 
+								items-center justify-center lg:text-sm lg:border-transparent"
+									>
+										Enable
+										<span className="pl-2">
+											<MdOutlineDomain />
+										</span>
+									</button>
+								)}
+							</>
+						</div>
+					) : null}
+				</div>
 			</div>
-			{(shelter.author.id !== userDetail?.id)&&<div className="flex items-center justify-between row relative min-w-full w-80 ">
-				<div className="fixed inset-x-0 bottom-0  sm:bottom-0 z-[61] lg:left-4 lg:bottom-2">
-				<button
-					className="my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-4 px-8  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
-					onClick={() => setIsOpenDonate(true)}>
-					Support Us!!
-				</button>
+			{shelter.author.id !== userDetail?.id && (
+				<div className="relative flex items-center justify-between min-w-full row w-80 ">
+					<div className="fixed inset-x-0 bottom-0  sm:bottom-0 z-[61] lg:left-4 lg:bottom-2">
+						<button
+							className="my-2 mx-auto w-full bg-[#ca7c62] text-gray-100 hover:text-white py-4 px-8  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
+							onClick={() => setIsOpenDonate(true)}
+						>
+							Support Us!!
+						</button>
+					</div>
+					<div className="fixed  inset-x-0 bottom-0 sm:bottom-0  z-[60] lg:left-4 lg:bottom-2">
+						<button className="my-2 mx-auto animate-custom-ping hover:animate-none w-full bg-[#ca7c62] text-gray-100 hover:text-white py-4 px-8  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300">
+							Support Us!!
+						</button>
+					</div>
 				</div>
-				<div className="fixed  inset-x-0 bottom-0 sm:bottom-0  z-[60] lg:left-4 lg:bottom-2">
-				<button
-					className="my-2 mx-auto animate-custom-ping hover:animate-none w-full bg-[#ca7c62] text-gray-100 hover:text-white py-4 px-8  rounded-lg tracking-wide font-bold  focus:outline-none focus:shadow-outline hover:bg-[#462312] shadow-lg cursor-pointer transition ease-in duration-300"
-					>
-					Support Us!!
-				</button>
-				</div>
-			</div>}
+			)}
 		</div>
 	)
 };
