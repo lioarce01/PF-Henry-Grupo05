2576db23524e9ec3699753835fc9ddadf3a9d8f6
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-28 13:56:09 -0300

    feat(client/src/redux/slices/manageShelter): added animals state

--- client/src/redux/slices/manageShelters/index.js
+++ client/src/redux/slices/manageShelters/index.js
@@ -5,18 +5,23 @@ export const manageSheltersSlice = createSlice({
     name: "manageShelters",
 
     initialState: {
-        carousel: ''
+        carousel: '',
+        animals: []
     },
 
     reducers: {
         carouselShelters: (state, action) => {
             state.carousel = action.payload
+        },
+        setAnimals: (state, action) => {
+            state.animals = action.payload
         }
+        
     }
 });
 
 // action creators are generated for each case reducer function
-export const { carouselShelters } = manageSheltersSlice.actions;
+export const { carouselShelters, setAnimals } = manageSheltersSlice.actions;
 export const selectShelters = state => state.manageShelters
 
 export default manageSheltersSlice.reducer;
