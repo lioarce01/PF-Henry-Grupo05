ca41bf8759900045df2e576b263e6deb402675ec
Ismox1440 <ismael.saragusti24@gmail.com>
2022-11-28 00:47:16 -0300

    feat(client/src/components): added darkmode in videoplayer and ticketspage

--- client/src/components/Tiket/index.jsx
+++ client/src/components/Tiket/index.jsx
@@ -11,9 +11,9 @@ function Ticket({ticket}) {
 
   return (
     <>
-      <div onClick={() => setIsOpen(true)} className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+      <div onClick={() => setIsOpen(true)} className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 dark:hover:bg-white rounded-md px-2 py-2 my-2">
         <span className={`${ticket.status === "for review" ? "bg-yellow-400" : "bg-green-400" } h-2 w-2 m-2 rounded-full`}></span>
-        <div className="flex-grow font-medium px-2">{location.pathname !== "/tickets" ? ticket.shelter.name : ticket.title }</div>
+        <div className="flex-grow font-medium px-2 dark:text-[#FF7272]">{location.pathname !== "/tickets" ? ticket.shelter.name : ticket.title }</div>
         <div className="text-sm font-normal text-gray-500 tracking-wide">
           {ticket.status}
         </div>
