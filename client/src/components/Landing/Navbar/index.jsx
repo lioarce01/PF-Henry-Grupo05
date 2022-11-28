3023e95bc1e858e38fbd657e19004fd9911459bb
JuanjoPischetz <juanjo.pischetz@gmail.com>
2022-11-28 20:09:15 -0300

    Lading: Responsive & Dark Mode

--- client/src/components/Landing/Navbar/index.jsx
+++ client/src/components/Landing/Navbar/index.jsx
@@ -7,21 +7,20 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between lg:px-10 px-3 py-5 items-center w-full z-50">
 
-      <div className={`flex group flex-row`}>
-        <span className={`lg:text-[2rem] font-bold text-[#201008] group-hover:text-[#FF7272] transition-all duration-500 font-mono mt-[2px]`}>Paws</span>
-        <span className={`lg:text-[2rem] font-bold text-[#FF7272] group-hover:text-[#201008] transition-all duration-500`}>Founding</span>
+      <div className={`flex group flex-row items-baseline`}>
+        <span className={`lg:text-[2rem] text-sm sm:text-xl font-bold text-[#201008] dark:text-[#F0EEEE] group-hover:text-[#FF7272] transition-all duration-500 font-mono `}>Paws</span>
+        <span className={`lg:text-[2rem] text-sm sm:text-xl font-bold text-[#FF7272] group-hover:text-[#201008] transition-all duration-500`}>Founding</span>
       </div>
 
 
       <div className="flex items-center">
         <div className="flex items-center">
-          <a className="font-bold mx-7 text-[#838788] link-underline link-underline-black lg:text-[1.2rem]" href="/about">About Us</a>
-          <a className="font-bold mx-7 text-[#838788] link-underline link-underline-black lg:text-[1.2rem]" href="/#">Contact</a>
+          <a className="font-bold text-[#838788] dark:text-[#AFB3B4] link-underline link-underline-black lg:text-[1.7rem] lg:mr-[40px] text-sm sm:text-xl" href="/about">About Us</a>
         </div>
       </div>
       <Link to='/'>
         <div>
-          <img className="lg:w-12 lg:h-12 w-12 h-8" src={logo} alt="LOGO" />
+          <img className="lg:w-12 lg:h-12 w-11 h-8" src={logo} alt="LOGO" />
         </div>
       </Link>
     </nav>
--- client/src/components/Landing/index.jsx
+++ client/src/components/Landing/index.jsx
@@ -9,11 +9,11 @@ function Landing() {
   
 
   return (
-    <div className="bg-[#FFF5F4]">
+    <div className="bg-[#FFF5F4] dark:bg-[#27242C]">
       <Navbar />
       <Introduction />
 
-      <div className="relative lg:top-[-140px] max-sm:hidden">
+      <div className="lg:mt-[150px] lg:mb-[200px] max-sm:hidden">
             <section className="demo section07" id="section07">
               <a href="#trending"><span></span><span></span><span></span></a>
             </section>
@@ -26,12 +26,12 @@ function Landing() {
       <div>
         <div className="block mx-auto mt-16">
 
-          <div className="flex flex-col items-center text-center mx-auto mt-20">
-            <h1 id="#trending" className="mb-8 w-max text-2xl font-bold leading-none tracking-tighter text-[#000] md:text-5xl lg:text-5xl">Helping animals around the globe.</h1>
+          <div className="flex flex-col items-center text-center mx-auto mt-20 p-4">
+            <h1 id="#trending" className="mb-4 text-2xl font-bold leading-none tracking-tighter text-[#000] dark:text-white md:text-5xl lg:text-5xl">Helping animals around the globe.</h1>
 
-            <p className="text-[#000] text-lg  md:text-3xl lg:text-3xl mt-10 w-full font-sans">At Paws Founding we know that NGOs and community shelters, usually have a really hard time raising money and lack tools to campaign for it.</p>
+            <p className="text-[#000] dark:text-[#F0EEEE] text-lg  md:text-3xl lg:text-3xl mt-10 w-full font-sans">At Paws Founding we know that NGOs and community shelters, usually have a really hard time raising money and lack tools to campaign for it.</p>
 
-            <p className="text-[#000] text-lg md:text-3xl lg:text-3xl mt-10 w-full">That's why our page is completely pro-bono, of course you can help us and them by donating to our campain: all of the extra profits will go to the least founded shelters in the page.</p>
+            <p className="text-[#000] dark:text-[#F0EEEE] text-lg md:text-3xl lg:text-3xl mt-10 w-full">That's why our page is completely pro-bono, of course you can help us and them by donating to our campain: all of the extra profits will go to the least founded shelters in the page.</p>
           </div>
 
         </div>
