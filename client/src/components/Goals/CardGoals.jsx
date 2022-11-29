1416fce13fd257cc3a72b0818b1f2ecabf533d94
lioarce01 <lioarce1@gmail.com>
2022-11-28 22:22:00 -0300

    fixed: only owner of shelter can see disable/enable button or delete

--- client/src/components/Goals/CardGoals.jsx
+++ client/src/components/Goals/CardGoals.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from 'react'
-import { useEnableGoalMutation, useDisableGoalMutation, useDeleteGoalMutation } from "../../redux/api/goals"
+import { useSelector } from "react-redux"
+import {
+	useEnableGoalMutation,
+	useDisableGoalMutation,
+	useDeleteGoalMutation,
+} from "../../redux/api/goals"
 import ModalDonate from "../OngProfile/Donate/ModalDonate"
 import "./CardGoals.css"
 
-const CardGoals = ({ title, content, goal, budget, shelterId, id, shelterName, enable, shelterRefetch }) => {
+const CardGoals = ({
+	title,
+	content,
+	goal,
+	budget,
+	shelterId,
+	id,
+	shelterName,
+	enable,
+	shelterRefetch,
+	authorId,
+}) => {
 	const [isOpenDonate, setIsOpenDonate] = useState(false)
 	const closeModalDonate = () => setIsOpenDonate(false)
 	const [disableGoal] = useDisableGoalMutation()
 	const [enableGoal] = useEnableGoalMutation()
 	const [deleteGoal] = useDeleteGoalMutation()
+	const { userDetail, isAuth } = useSelector(
+		(state) => state.localStorage.userState
+	)
+
+	console.log("authorId: ", authorId)
 
 	const handleEnable = async () => {
 		await enableGoal({ id })
@@ -26,15 +47,21 @@ const CardGoals = ({ title, content, goal, budget, shelterId, id, shelterName, e
 	}
 
 	return (
-		<div className="flex flex-col w-[280px] gap-2 p-[25px] bg-white shadow-[6px_16px_54px_-27px_rgba(133,133,133,0.7)]
+		<div
+			className="flex flex-col w-[280px] gap-2 p-[25px] bg-white shadow-[6px_16px_54px_-27px_rgba(133,133,133,0.7)]
 						transition-all duration-300 hover:mt-[10px] hover:bg-[#fcfcfc] rounded-xl dark:bg-[#1B1A1F]
-						dark:shadow-[6px_16px_54px_-27px_rgba(133,133,133,0.4)] dark:hover:bg-[#1f1e24]">
-
-			<h1 className="text-lg font-semibold text-[#838788] dark:text-[#b3b8b9]">{title}</h1>
+						dark:shadow-[6px_16px_54px_-27px_rgba(133,133,133,0.4)] dark:hover:bg-[#1f1e24]"
+		>
+			<h1 className="text-lg font-semibold text-[#838788] dark:text-[#b3b8b9]">
+				{title}
+			</h1>
 			<p className="text-[#ACB1B2]">{content}</p>
 
 			<div className="flex flex-col items-center mt-[20px]">
-				<label htmlFor="goal" className="flex flex-row items-center gap-1 dark:text-[#b3b8b9] mb-[2px]">
+				<label
+					htmlFor="goal"
+					className="flex flex-row items-center gap-1 dark:text-[#b3b8b9] mb-[2px]"
+				>
 					{budget / 100}% <span>completed</span>
 				</label>
 
@@ -53,33 +80,37 @@ const CardGoals = ({ title, content, goal, budget, shelterId, id, shelterName, e
 				) : (
 					<button
 						onClick={() => setIsOpenDonate(true)}
-						className="px-2 py-1 text-white transition duration-300 rounded-md bg-[#6371f1] hover:bg-[#535fd1] dark:bg-[#7580e4] dark:hover:bg-[#6a75d3]"
+						className="px-2 py-1 text-white transition duration-300 rounded-md bg-[#6371f1] hover:bg-[#535fd1] dark:bg-[#7580e4] dark:hover:bg-[#6a75d3] line-through"
 						disabled
 					>
 						Help us!
 					</button>
 				)}
-				{enable ? (
+				{authorId === userDetail?.id || userDetail?.role === "Admin" ? (
+					enable ? (
+						<button
+							className="px-2 py-1 text-white transition duration-300 rounded-md outline-none bg-[#FF7272] hover:bg-[#e76464]"
+							onClick={handleDisable}
+						>
+							Disable
+						</button>
+					) : (
+						<button
+							className="px-2 py-1 text-white transition duration-300 rounded-md outline-none bg-[#6371f1] hover:bg-[#535fd1]"
+							onClick={handleEnable}
+						>
+							Enable
+						</button>
+					)
+				) : null}
+				{authorId === userDetail?.id || userDetail?.role === "Admin" ? (
 					<button
-						className="px-2 py-1 text-white transition duration-300 rounded-md outline-none bg-[#FF7272] hover:bg-[#e76464]"
-						onClick={handleDisable}
+						className="px-2 py-1 text-white transition duration-300 rounded-md bg-[#FF7272] hover:bg-[#e76464]"
+						onClick={handleDelete}
 					>
-						Disable
+						Delete
 					</button>
-				) : (
-					<button
-						className="px-2 py-1 text-white transition duration-300 rounded-md outline-none bg-[#6371f1] hover:bg-[#535fd1]"
-						onClick={handleEnable}
-					>
-						Enable
-					</button>
-				)}
-				<button
-					className="px-2 py-1 text-white transition duration-300 rounded-md bg-[#FF7272] hover:bg-[#e76464]"
-					onClick={handleDelete}
-				>
-					Delete
-				</button>
+				) : null}
 			</div>
 			<ModalDonate
 				isOpen={isOpenDonate}
