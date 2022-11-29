bd261cf1fa0ef28758fd857ef6e5646f00be2acd
nllosas <nllosas@fi.uba.ar>
2022-11-29 20:16:30 -0300

    hotfix to about us page

--- client/src/components/About/TeamCard/TeamCard.jsx
+++ client/src/components/About/TeamCard/TeamCard.jsx
@@ -4,7 +4,7 @@ import LinkedinBtn from './LinkedinBtn';
 
 const TeamCard = ({name,image,linkedin,github,phrase}) => {
     return ( 
-        <div className="mb-12 lg:mb-12 p-4 hover:scale-110 duration-200 dark:bg-[#AFB3B4] bg-white shadow-xl dark:shadow-[rgb(77,77,77)] shadow-[rgb(255,213,201)] rounded-2xl w-56">
+        <div className="mb-12 lg:mb-12 p-4 hover:scale-110 duration-200 dark:bg-[#1B1A1F] bg-white shadow-xl dark:shadow-[rgb(77,77,77)] shadow-[rgb(255,213,201)] rounded-2xl w-56">
             <img
               className="rounded-lg shadow-lg mb-3 mx-auto  object-cover"
               src={image}
@@ -12,7 +12,7 @@ const TeamCard = ({name,image,linkedin,github,phrase}) => {
               style={{ width: "180px", height: '180px' }}
             />
             <h5 className="text-lg font-bold mb-2 text-[#F87171]">{name}</h5>
-            <p className="mb-3">{phrase}</p>
+            <p className="mb-3 dark:text-[#F0EEEE]">{phrase}</p>
             <ul className="list-inside flex mx-auto justify-center">
                 <GithubBtn github={github}/>   
                 <LinkedinBtn linkedin={linkedin}/>
