0d6285e160237e43bfa08d6b86b5f5ee3d49bc63
lioarce01 <lioarce1@gmail.com>
2022-11-30 19:38:19 -0300

    fix(src/components/about & Footer): fixed footer darkmode/lightmode

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -8,19 +8,18 @@ const About = () => {
   
 
   return (
-    <div>
+
       <div className="dark:bg-[#27242C] bg-[#EFF0F3]">
         <Navbar />
         <div className="w-full dark:bg-[#27242C] bg-[#EFF0F3] h-[1px]"/>
           <h1 className="mt-8 text-center text-5xl font-bold text-[#462312] mb-12 dark:text-[#F0EEEE]">About Us</h1>
-          <div className="flex flex-row flex-wrap gap-x-32 gap-y-16 justify-items-center justify-center">
+          <div className="flex flex-row flex-wrap justify-center gap-x-32 gap-y-16 justify-items-center">
             {team.map((person, index) => <TeamCard key={index} github={person.github} linkedin={person.linkedin} image={person.image} name={person.name} phrase={person.phrase} />)}
         </div>
+        <Footer/>
       </div>
-      <Footer/>
-    </div>
   );
 };
-// className="flex flex-row flex-wrap m-10 gap-x-32 gap-y-16 justify-items-center justify-around"
-//className="grid md:grid-cols-2 lg:grid-cols-4 gap-x-5 lg:gap-xl-9 gap-y-16 px-8 mx-auto justify-items-center"
+// className="flex flex-row flex-wrap justify-around m-10 gap-x-32 gap-y-16 justify-items-center"
+//className="grid px-8 mx-auto md:grid-cols-2 lg:grid-cols-4 gap-x-5 lg:gap-xl-9 gap-y-16 justify-items-center"
 export default About;
